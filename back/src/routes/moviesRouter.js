5212955eeebf138de92752c105e89cc273fa7fc0
Jose Andres Borrero <151858697+andreschunior@users.noreply.github.com>
2024-02-26 21:06:12 -0400

    Delete back directory

--- back/src/routes/moviesRouter.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express');
-const moviesRouter = Router();
-const { controllerMovies } = require('../controllers/moviesControllers');
-
-moviesRouter.get('/', controllerMovies);
-
-module.exports = moviesRouter;
